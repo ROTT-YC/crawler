a64157f05b63e682e94e9380321aca34e8392001
ROTT-YC <66822676+ROTT-YC@users.noreply.github.com>
2024-06-05 14:44:25 +0800

    Delete class_india directory

--- class_india/package/__init__.py
+++
@@ -1,4 +0,0 @@
-from .generator_gpt import genereate_content
-from .pic_modify import load_pic
-from .watermark import draw_text, draw_icon
-from .get_time import currentime
--- class_india/setting.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-def env_str():
-    global ai_key 
-    ai_key= "Openai API Key"
-
-    global today_date
-    today_date = str(datetime.date.today())
